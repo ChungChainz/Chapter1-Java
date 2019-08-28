38bd98d300470ced296e2848996a9d97ea38330e
Chris Molis <chrismolis12@gmail.com>
2019-08-28 08:36:24 -0500

    commit

---
+++ Chapter1/src/TablesAndChairs.java
@@ -0,0 +1,22 @@
+public class TablesAndChairs {
+    public static void main (String[] args){
+        System.out.println("C                                                                            C");
+        System.out.println("C                                                                            C");
+        System.out.println("C                                                                            C");
+        System.out.println("C                                                                            C");
+        System.out.println("C                                                                            C");
+        System.out.println("C                                                                            C");
+        System.out.println("C                                                                            C");
+        System.out.println("C                                                                            C");
+        System.out.println("SEATSEATSEATSEATSEAT         TABLETABLETABLETABLE        SEATSEATSEATSEATSEAT");
+        System.out.println("L                  L         L                  L        L                   L");
+        System.out.println("L                  L         L                  L        L                   L");
+        System.out.println("L                  L         L                  L        L                   L");
+        System.out.println("L                  L         L                  L        L                   L");
+        System.out.println("L                  L         L                  L        L                   L");
+        System.out.println("L                  L         L                  L        L                   L");
+        System.out.println("L                  L         L                  L        L                   L");
+        System.out.println("L                  L         L                  L        L                   L");
+
+    }
+}
---
+++ Chapter1/src/TacoTruck.java
@@ -0,0 +1,23 @@
+public class TacoTruck {
+
+    public static void main(String[] args) {
+        System.out.println("                    __________________________________________");
+        System.out.println("                    1                                        1");
+        System.out.println("                    1              TACO TRUCK                1");
+        System.out.println("                    1#######    ##################           1");
+        System.out.println("                    1WINDOW#    WINDOWWINDOWWINDOW           1");
+        System.out.println("                    1WINDOW#    WINDOWWINDOWWINDOW           1");
+        System.out.println("                    1#######    ##################           1");
+        System.out.println("                    1                                        1");
+        System.out.println("   VVVVVVVVVVVVVVVVVV ___________________                    l");
+        System.out.println("   1                  1_______   _______1                    l");
+        System.out.println("   1                         1  1                            l");
+        System.out.println("   1    __________           1  1                   __________1");
+        System.out.println("   1  1 0000000000 1         1__1  acos!          1 0000000000");
+        System.out.println("   1  1 00XXXXXX00 1                              1 00XXXXXX00");
+        System.out.println("   1__1 00XXXXXX00 1______________________________1 00XXXXXX00");
+        System.out.println("        0000000000                                  0000000000 ");
+    }
+
+
+}
